67ecf0038261e748186948cf415ca96102f99a0f
wuwenbang <wuwenbang@foxmail.com>
2020-07-31 09:04:53 +0800

    抽离svg至icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,11 +1,29 @@
 <template>
-  <button class="lm-button">
-    button
+
+  <button class="lm-button" :class="{[`icon-${iconPosition}`]:true}">
+    <lm-icon v-if="icon" :name="icon"></lm-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
+
 <script>
-export default {};
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      // 属性检查器
+      validator(value) {
+        return value == "left" || value == "right";
+      },
+    },
+  },
+};
 </script>
+
 <style lang="scss">
 .lm-button {
   height: var(--button-height);
@@ -14,6 +32,10 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -24,6 +46,23 @@ export default {};
 
   &:focus {
     outline: none;
+  }
+  .content {
+    order: 2;
+  }
+  .icon {
+    order: 1;
+    margin-right: 4px;
+  }
+  &.icon-right {
+    .content {
+      order: 1;
+    }
+    .icon {
+      order: 2;
+      margin-left: 4px;
+      margin-right: 0;
+    }
   }
 }
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="`#icon-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
