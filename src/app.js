8ac5b541f2ed9ec1cda3d40afd548bff195710f4
wuwenbang <wuwenbang@foxmail.com>
2020-07-31 22:16:38 +0800

    创建button-group组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('lm-button', Button)
 Vue.component('lm-icon', Icon)
+Vue.component('lm-button-group', ButtonGroup)
 new Vue({
     el: '#app',
     data: {
---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="lm-button-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.lm-button-group {
+  vertical-align: middle;
+  display: inline-flex;
+
+  > .lm-button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
