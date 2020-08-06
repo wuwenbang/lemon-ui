62b1193355b7afbeb736ca412ccf3ae17a84e812
wuwenbang <wuwenbang@foxmail.com>
2020-08-06 10:30:30 +0800

    基本实现row和col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]" :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+    <div style="border: 1px solid green; height : 100px;">
+      <slot></slot>
+
+    </div>
   </div>
 </template>
 
@@ -8,7 +11,15 @@
 export default {
   name: "LemonCol",
   props: {
-    span: [Number, String],
+    span: {
+      type: [Number, String],
+    },
+    offset: {
+      type: [Number, String],
+    },
+  },
+  data() {
+    return { gutter: 0 };
   },
 };
 </script>
@@ -17,13 +28,16 @@ export default {
 .col {
   height: 100px;
   width: 50%;
-  border: 1px solid gray;
-  background: #ccc;
-
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
