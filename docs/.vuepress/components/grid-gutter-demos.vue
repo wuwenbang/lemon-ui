ee37daba0d5da3a961b090de5a136f4d9b33e6fa
wuwenbang <wuwenbang@foxmail.com>
2020-08-17 21:00:38 +0800

    更新grid文档样式

--- docs/.vuepress/components/grid-gutter-demos.vue
+++ docs/.vuepress/components/grid-gutter-demos.vue
@@ -3,27 +3,27 @@
     <div class="row-bg">
       <Row :gutter="10">
         <Col :span="8">
-        <div class="content bg-purple"></div>
+        <div class="content bg-color"></div>
         </Col>
         <Col :span="8">
-        <div class="content bg-purple-light"></div>
+        <div class="content bg-color-light"></div>
         </Col>
         <Col :span="8">
-        <div class="content bg-purple"></div>
+        <div class="content bg-color"></div>
         </Col>
       </Row>
       <Row :gutter="10">
         <Col :span="6">
-        <div class="content bg-purple"></div>
+        <div class="content bg-color"></div>
         </Col>
         <Col :span="6">
-        <div class="content bg-purple-light"></div>
+        <div class="content bg-color-light"></div>
         </Col>
         <Col :span="6">
-        <div class="content bg-purple"></div>
+        <div class="content bg-color"></div>
         </Col>
         <Col :span="6">
-        <div class="content bg-purple-light"></div>
+        <div class="content bg-color-light"></div>
         </Col>
       </Row>
     </div>
@@ -48,45 +48,45 @@ export default {
       code: `
 <div class="row-bg">
     <Row :gutter="20">
-        <Col :span="8"><div class="content bg-purple"></div></Col>
-        <Col :span="8"><div class="content bg-purple-light"></div></Col>
-        <Col :span="8"><div class="content bg-purple"></div></Col>
+        <Col :span="8"><div class="content bg-color"></div></Col>
+        <Col :span="8"><div class="content bg-color-light"></div></Col>
+        <Col :span="8"><div class="content bg-color"></div></Col>
     </Row>
     <Row :gutter="20">
-        <Col :span="6"><div class="content bg-purple"></div></Col>
-        <Col :span="6"><div class="content bg-purple-light"></div></Col>
-        <Col :span="6"><div class="content bg-purple"></div></Col>
-        <Col :span="6"><div class="content bg-purple-light"></div></Col>
+        <Col :span="6"><div class="content bg-color"></div></Col>
+        <Col :span="6"><div class="content bg-color-light"></div></Col>
+        <Col :span="6"><div class="content bg-color"></div></Col>
+        <Col :span="6"><div class="content bg-color-light"></div></Col>
     </Row>
 </div>
-<style scoped>
-    .row {
-        margin-bottom: 20px;
-        height: 20px;
-    }
-    .row:last-child {
-            margin-bottom: 0;
-        }
-    .col {
-        border-radius: 4px;
-    }
-    .bg-purple-dark {
-        background: #99a9bf;
-    }
-    .bg-purple {
-        background: #d3dce6;
-    }
-    .bg-purple-light {
-        background: #e5e9f2;
-    }
-    .content {
-        border-radius: 4px;
-        min-height: 36px;
-        width: 100%;
-    }
-    .row-bg {
-        padding: 10px 0;
-    }
+<style lang="scss" scoped>
+.row {
+  margin-bottom: 20px;
+  height: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.col {
+  border-radius: 4px;
+}
+.bg-color-dark {
+  background: hsl(150, 50%, 50%);
+}
+.bg-color {
+  background: hsl(150, 50%, 60%);
+}
+.bg-color-light {
+  background: hsl(150, 50%, 80%);
+}
+.content {
+  border-radius: 4px;
+  min-height: 36px;
+  width: 100%;
+}
+.row-bg {
+  padding: 10px 0;
+}
 </style>
 `,
     };
@@ -105,14 +105,14 @@ export default {
 .col {
   border-radius: 4px;
 }
-.bg-purple-dark {
-  background: #99a9bf;
+.bg-color-dark {
+  background: hsl(150, 50%, 50%);
 }
-.bg-purple {
-  background: #d3dce6;
+.bg-color {
+  background: hsl(150, 50%, 60%);
 }
-.bg-purple-light {
-  background: #e5e9f2;
+.bg-color-light {
+  background: hsl(150, 50%, 80%);
 }
 .content {
   border-radius: 4px;
